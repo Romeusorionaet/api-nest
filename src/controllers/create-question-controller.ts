1f34d7c2b4ea4d9f45e5b071a296bc48dd53ae96
romeusorionaet <romeusoares14569@gmail.com>
2024-02-27 15:37:43 -0300

    create decorator of authentication

--- src/controllers/create-question-controller.ts
+++ src/controllers/create-question-controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { CurrentUser } from "src/auth/corrent-user-decorator";
+import { UserPayload } from "src/auth/jwt.strategy";
 
 @Controller("/question")
 @UseGuards(AuthGuard("jwt"))
@@ -7,7 +9,9 @@ export class CreateQuestionController {
   constructor() {}
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user.sub);
+
     return "ok";
   }
 }
