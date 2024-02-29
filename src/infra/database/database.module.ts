68f7a1fb50fc285b881eba4074d7f3b59f05c978
romeusorionaet <romeusoares14569@gmail.com>
2024-02-29 14:01:02 -0300

    implements repositories

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,30 @@
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
+import { PrismaQuestionsRepository } from "./prisma/repositories/prisma-questions-repository";
+import { PrismaQuestionCommentsRepository } from "./prisma/repositories/prisma-question-comments-repository";
+import { PrismaQuestionAttachmentsRepository } from "./prisma/repositories/prisma-question-attachment-repository";
+import { PrismaAnswerAttachmentsRepository } from "./prisma/repositories/prisma-answer-attachment-repository";
+import { PrismaAnswerCommentsRepository } from "./prisma/repositories/prisma-answer-comments-repository";
+import { PrismaAnswersRepository } from "./prisma/repositories/prisma-answers-repository";
+
+@Module({
+  providers: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+  ],
+})
+export class DatabaseModule {}
