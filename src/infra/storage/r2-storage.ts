2cc2157167b74448d06093aef75d3c40c707f6f7
romeusorionaet <romeusoares14569@gmail.com>
2024-03-22 14:51:58 -0300

    integrate with Cloudflare R2

---
+++ src/infra/storage/r2-storage.ts
@@ -0,0 +1,50 @@
+import {
+  UploadParams,
+  Uploader,
+} from "@/domain/forum/application/storage/uploader";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { EnvService } from "../env/env.service";
+import { randomUUID } from "node:crypto";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class R2Storage implements Uploader {
+  private client: S3Client;
+
+  constructor(private envService: EnvService) {
+    const accountId = envService.get("CLOUDFLARE_ACCOUNT_ID");
+    const accessKeyId = envService.get("AWS_ACCESS_KEY_ID");
+    const secretAccessKey = envService.get("AWS_SECRET_ACCESS_KEY");
+
+    this.client = new S3Client({
+      endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
+      region: "auto",
+      credentials: {
+        accessKeyId,
+        secretAccessKey,
+      },
+    });
+  }
+
+  async upload({
+    fileName,
+    fileType,
+    body,
+  }: UploadParams): Promise<{ url: string }> {
+    const uploadId = randomUUID();
+    const uniqueFileName = `${uploadId}-${fileName}`;
+
+    await this.client.send(
+      new PutObjectCommand({
+        Bucket: this.envService.get("AWS_BUCKET_NAME"),
+        Key: uniqueFileName,
+        ContentType: fileType,
+        Body: body,
+      }),
+    );
+
+    return {
+      url: uniqueFileName,
+    };
+  }
+}
---
+++ src/infra/storage/storage.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { R2Storage } from "./r2-storage";
+import { Uploader } from "@/domain/forum/application/storage/uploader";
+import { EnvModule } from "../env/env.module";
+
+@Module({
+  imports: [EnvModule],
+  providers: [
+    {
+      provide: Uploader,
+      useClass: R2Storage,
+    },
+  ],
+  exports: [Uploader],
+})
+export class StorageModule {}
