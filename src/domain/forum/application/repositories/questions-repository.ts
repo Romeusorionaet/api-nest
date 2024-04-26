1ec625c65cddadfe5d0b1e948680c02803b76315
romeusorionaet <romeusoares14569@gmail.com>
2024-04-26 15:16:16 -0300

    value object question details

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,10 +1,12 @@
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Question } from "@/domain/forum/enterprise/entities/question";
+import { QuestionDetails } from "../../enterprise/entities/value-objects/question-details";
 
 export abstract class QuestionsRepository {
   abstract create(question: Question): Promise<void>;
   abstract findById(id: string): Promise<Question | null>;
   abstract findBySlug(slug: string): Promise<Question | null>;
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>;
   abstract delete(question: Question): Promise<void>;
   abstract save(question: Question): Promise<void>;
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>;
